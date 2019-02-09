e71557a4bb521addc73470d5a1c784cef10a19f1
Peltoche <dev@halium.fr>
2019-02-09 17:00:14 +0100

    Implement Mock.GetAllClientsOnTopic

--- storage/mock.go
+++ storage/mock.go
@@ -16,3 +16,14 @@ type Mock struct {
 func (t *Mock) RegisterNewClient(ctx context.Context, client *model.Client) error {
 	return t.Called(client).Error(0)
 }
+
+// GetAllClientsOnTopic method mock.
+func (t *Mock) GetAllClientsOnTopic(ctx context.Context, topicName string) ([]model.Client, error) {
+	args := t.Called(topicName)
+
+	if args.Get(0) == nil {
+		return nil, args.Error(1)
+	}
+
+	return args.Get(0).([]model.Client), args.Error(1)
+}
