216a1f34e3614f1cfdabe530a7a75191c1b27d7e
Peltoche <dev@halium.fr>
2019-02-09 16:58:25 +0100

    Add some tests on InMemory.GetAllClientsOnTopic

--- storage/in_memory_test.go
+++ storage/in_memory_test.go
@@ -79,3 +79,48 @@ func Test_InMemory_GetAllClientOnTopic_success(t *testing.T) {
 	require.NoError(t, err)
 	assert.EqualValues(t, []model.Client{client}, res)
 }
+
+func Test_InMemory_GetAllClientOnTopic_success_2(t *testing.T) {
+	client := model.Client{
+		ID:          "some-id",
+		Topic:       "some-topic",
+		Application: "some-app",
+		Action:      "read",
+		Subject:     "my-avro-subject",
+		Version:     "2",
+	}
+	client2 := model.Client{
+		ID:          "some-other-id",
+		Topic:       "some-other-topic",
+		Application: "some-app",
+		Action:      "read",
+		Subject:     "my-other-avro-subject",
+		Version:     "1",
+	}
+
+	storage := NewInMemory()
+
+	err := storage.RegisterNewClient(context.Background(), &client)
+	require.NoError(t, err)
+
+	err = storage.RegisterNewClient(context.Background(), &client2)
+	require.NoError(t, err)
+
+	// Check for "some-topic"
+	res, err := storage.GetAllClientsOnTopic(context.Background(), "some-topic")
+	require.NoError(t, err)
+	assert.EqualValues(t, []model.Client{client}, res)
+
+	// Check for "some-other-topic"
+	res, err = storage.GetAllClientsOnTopic(context.Background(), "some-other-topic")
+	require.NoError(t, err)
+	assert.EqualValues(t, []model.Client{client2}, res)
+}
+
+func Test_InMemory_GetAllClientOnTopic_with_no_client_found(t *testing.T) {
+	storage := NewInMemory()
+
+	res, err := storage.GetAllClientsOnTopic(context.Background(), "some-topic")
+	require.NoError(t, err)
+	assert.Empty(t, res)
+}
